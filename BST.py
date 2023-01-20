77d89c1845b15a1b6234292701871af06d7249c9
Siana Kong <Siana Kong>
2023-01-20 13:50:55 -0500

    added delete function to BST

--- BST.py
+++ BST.py
@@ -8,31 +8,58 @@ class BST:
     def __init__(self):
         self.root = None
 
-    def insert(self, new_node, node=None):
+    #O(h): where h is the height of the tree
+    #O(log n): for a well-balanced tree
+    def insert(self, val, node=None):
         if not node:
             node = self.root
         if not node:
-            self.root = new_node
+            self.root = Node(val)
             return
-        if new_node.val < node.val:
+        if val < node.val:
             if not node.left:
-                node.left = new_node
+                node.left = Node(val)
             else:
-                self.insert(new_node, node.left)
+                self.insert(val, node.left)
         else:
             if not node.right:
-                node.right = new_node
+                node.right = Node(val)
             else:
-                self.insert(new_node, node.right)
+                self.insert(val, node.right)
 
-    def search(self, search_node, node=None):
+    #O(h): where h is the height of the tree
+    #O(log n): for a well-balanced tree
+    def search(self, val, node=None):
         if not node:
             node = self.root
         if not node:
             return False
-        if search_node.val == node.val:
+        if val == node.val:
             return True
-        elif search_node.val < node.val:
-            return self.search(search_node, node.left)
+        elif val < node.val:
+            return self.search(val, node.left)
         else:
-            return self.search(search_node, node.right)
+            return self.search(val, node.right)
+            
+    #O(h): where h is the height of the tree
+    def delete(self, val, node=None):
+        if not node:
+            node = self.root
+        if not node:
+            return None
+        if val < node.val:
+            node.left = self.delete(val, node.left)
+        elif val > node.val:
+            node.right = self.delete(val, node.right)
+        else:
+            if not node.left and not node.right:
+                return None
+            elif not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+            else:
+                min_val = self.find_min(node.right)
+                node.val = min_val
+                node.right = self.delete(min_val, node.right)
+        return node
