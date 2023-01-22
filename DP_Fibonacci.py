9d232ea2d148cf975d62ad4770636312664edfe6
Siana Kong <Siana Kong>
2023-01-22 14:42:21 -0500

    adjust fibonacci algorithm to start from 1, not 0

--- DP_Fibonacci.py
+++ DP_Fibonacci.py
@@ -2,13 +2,16 @@
 
 def fib(n):
     # Initialize an array to store the Fibonacci numbers
-    fib_numbers = [0, 1]
-    # Base case: If n is 0 or 1, return n
+    fib_numbers = [1, 1]
+    # Base case: If n is 1 or 2, return n
     if n < 2:
-        return n
+        return 1
+    else:
     # Iterate from 2 to n
-    for i in range(2, n + 1):
-        # Compute the next Fibonacci number
-        fib_numbers.append(fib_numbers[i - 1] + fib_numbers[i - 2])
+        for i in range(2, n):
+            # Compute the next Fibonacci number
+            fib_numbers.append(fib_numbers[i - 1] + fib_numbers[i - 2])
     # Return the n-th Fibonacci number
-    return fib_numbers[n]
+    return fib_numbers[n-1]
+
+print(fib(6))
